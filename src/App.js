92b86bac73bf36002dffe75de661e29d70dd2213
JCameren <johnsonm.cameren@gmail.com>
2022-07-27 14:57:43 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col, Button, Alert, Breadcrumb, BreadcrumbItem, Card, Form} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProfilePic from '/home/syogeki/react-bootstrap-tutorial/src/imgs/chibi-me.png'
 
 function App() {
   return (
     <div className="App">
+
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <Form>
+        <Form.Group>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type="email" placeholder="example@email.com" />
+          <Form.Text className='text-muted'>
+            We'll never share your email address, trust us!
+          </Form.Text>
+        </Form.Group>
+        <Button variant='secondary' type='submit'>Submit</Button>
+      </Form>
+        <Card className="mt-5" style={{color: '#000', marginBottom: 15 }}>
+          <Card.Img src={ProfilePic}/>
+          <Card.Body>
+            <Card.Title>
+              Card Example
+            </Card.Title>
+            <Card.Text>
+              This is an example of react bootstrap cards
+            </Card.Text>
+            <Button variant='primary'>Read More</Button>
+          </Card.Body>
+        </Card>
+        <Breadcrumb>
+          <BreadcrumbItem>Test</BreadcrumbItem>
+          <BreadcrumbItem>Test 2</BreadcrumbItem>
+          <BreadcrumbItem>Test 3</BreadcrumbItem>
+        </Breadcrumb>
+      <Alert variant="secondary">This is a button</Alert>
+        <Button>Test Button</Button>
       </header>
     </div>
   );
